cb5df5d45f51ba5d1c9bdf0c9001a83bf526e60e
GabeBecker2048 <gabebecker2048@gmail.com>
2024-12-17 20:57:29 -0600

    Add extra fields to setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,29 @@ from setuptools import setup
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
+with open('README.md') as f:
+    README = f.read()
+
 setup(
     name="chessql",
     version="0.1.0",
+    url="https://github.com/GabeBecker2048/chessql",
+    description="A Python library for playing, visualizing, and storing chess games.",
+    author="Gabe Becker",
+    author_email="gabebecker2048@gmail.com",
+    long_description=README,
+    long_description_content_type='text/markdown',
+    license="MIT",
     packages=["chessql"],
     include_package_data=True,
     install_requires=requirements,
     package_data={
-        '': ['data/*.sql', 'data/*.ttf', 'data/img/*.png'],
+        "": ["data/*.sql", "data/*.ttf", "data/img/*.png"],
     },
+    python_requires='>=3.7',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
--- tester.py
+++ tester.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-g = Game()
+g = Game(white_name="Bob", black_name="Phil")
 
 g.move('d4',img=True).show()
