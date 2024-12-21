01ea755a5e38246a60b2a9b908a20213a1f6f51b
GabeBecker2048 <gabebecker2048@gmail.com>
2024-12-21 14:09:17 -0600

    fix setup

--- setup.py
+++ setup.py
@@ -3,10 +3,17 @@ from setuptools import setup
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
+with open('requirements-postgres.txt') as f:
+    requirements_postgres = f.read().splitlines()
+
+with open('requirements-postgres-binary.txt') as f:
+    requirements_postgres_binary = f.read().splitlines()
+
 with open('README.md') as f:
     README = f.read()
 
 setup(
+    name="ChessPy",
     version="0.1.0",
     url="https://github.com/GabeBecker2048/chessql",
     description="A Python library for playing, visualizing, and storing chess games.",
@@ -15,9 +22,15 @@ setup(
     long_description=README,
     long_description_content_type='text/markdown',
     license="MIT",
+    packages=["ChessPy"],
     include_package_data=True,
     install_requires=requirements,
     package_data={
+        "chesslib/data": ["*.sql", "*.ttf", "img/*.png"],
+    },
+    extras_require={
+        "postgres": requirements_postgres,
+        "postgres-binary": requirements_postgres_binary,
     },
     python_requires='>=3.9',
     classifiers=[
