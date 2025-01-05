050e3a662d7129358c7ca60ddd40ca6c557970ac
GabeBecker2048 <gabebecker2048@gmail.com>
2025-01-05 01:48:37 -0600

    Added postgres.Game documentation

--- VersionHistory.md
+++ VersionHistory.md
@@ -1,15 +1,16 @@
 # Project Version History
 
-| Version | Release Date | Changes/Notes                                    | Author        | Status   |
-|---------|--------------|--------------------------------------------------|---------------|----------|
-| 0.1.0   | 2024-12-18   | Initial release.                                 | Gabe Becker   | Released |
-| 0.2.0   | 2024-12-21   | Bug fixes and refactoring                        | Gabe Becker   | Released |
-| 0.3.1   | 2024-12-21   | Initial release to pypi; bug fixes               | Gabe Becker   | Released |
-| 0.3.2   | 2024-12-23   | bug fixes                                        | Gabe Becker   | Released |
-| 0.3.3   | 2024-12-23   | bug fixes                                        | Gabe Becker   | Released |
-| 0.3.4   | 2024-12-23   | bug fixes                                        | Gabe Becker   | Released |
-| 0.4.0   | 2025-01-05   | Added documentation to code                      | Gabe Becker   | Released |
-| 0.5.0   | 2025-01-05   | Major postgres fixes, changes, and documentation | Gabe Becker   | Latest   |
+| Version | Release Date | Changes/Notes                                    | Author        | Status     |
+|---------|--------------|--------------------------------------------------|---------------|------------|
+| 0.1.0   | 2024-12-18   | Initial release.                                 | Gabe Becker   | Released   |
+| 0.2.0   | 2024-12-21   | Bug fixes and refactoring                        | Gabe Becker   | Released   |
+| 0.3.1   | 2024-12-21   | Initial release to pypi; bug fixes               | Gabe Becker   | Released   |
+| 0.3.2   | 2024-12-23   | bug fixes                                        | Gabe Becker   | Released   |
+| 0.3.3   | 2024-12-23   | bug fixes                                        | Gabe Becker   | Released   |
+| 0.3.4   | 2024-12-23   | bug fixes                                        | Gabe Becker   | Released   |
+| 0.4.0   | 2025-01-05   | Added documentation to code                      | Gabe Becker   | Released   |
+| 0.5.0   | 2025-01-05   | Major postgres fixes, changes, and documentation | Gabe Becker   | Released   |
+| 0.5.1   | 2025-01-05   | postgres.Game documentation                      | Gabe Becker   | Latest     |
 
 # Planned Releases
 
--- chesssnake/postgres/Game.py
+++ chesssnake/postgres/Game.py
@@ -6,6 +6,40 @@ from ..chesslib import Chess
 
 
 class Game(BaseGame):
+    """
+    Extends the functionality of the `Game` class from `chesssnake.chesslib` to include SQL database support.
+
+    The `Game` class manages chess gameplay logic while adding functionality for game persistence and interaction with an SQL database.
+    Games can be loaded, saved, and automatically updated in the database.
+
+    :param white_id: ID of the player playing as white. Default is 0.
+    :type white_id: int
+    :param black_id: ID of the player playing as black. Default is 1.
+    :type black_id: int
+    :param group_id: Group or game ID. Default is 0.
+    :type group_id: int
+    :param white_name: Name of the white player. Default is an empty string ('').
+    :type white_name: str
+    :param black_name: Name of the black player. Default is an empty string ('').
+    :type black_name: str
+    :param sql: Whether to enable SQL support. Default is False.
+    :type sql: bool
+    :param auto_sql: Whether to automatically update the database after every state change. Default is False.
+    :type auto_sql: bool
+    :param db_conn_str: Database connection string. Default is None.
+    :type db_conn_str: str or None
+    :param db_name: Database name. Default is None.
+    :type db_name: str or None
+    :param db_user: Database username. Default is None.
+    :type db_user: str or None
+    :param db_password: Database password. Default is None.
+    :type db_password: str or None
+    :param db_host: Database host address. Default is None.
+    :type db_host: str or None
+    :param db_port: Database port number. Default is None.
+    :type db_port: str or None
+    """
+
     # this loads game data from the database into the game object
     # if the game does not exist in the database, a new one is created
     def __init__(self,
@@ -22,6 +56,39 @@ class Game(BaseGame):
                  db_password: str|None=None,
                  db_host: str|None=None,
                  db_port: str|None=None):
+        """
+        Initializes a new chess game and synchronizes with the SQL database if required.
+
+        If `sql` or `auto_sql` is enabled, it checks the database for a game with the given IDs. If a game exists, it loads
+        the data into memory. Otherwise, it creates a new record in the database and initializes the game in memory.
+
+        :param white_id: ID of the player playing as white. Default is 0.
+        :type white_id: int
+        :param black_id: ID of the player playing as black. Default is 1.
+        :type black_id: int
+        :param group_id: Group or game ID. Default is 0.
+        :type group_id: int
+        :param white_name: Name of the white player. Default is an empty string ('').
+        :type white_name: str
+        :param black_name: Name of the black player. Default is an empty string ('').
+        :type black_name: str
+        :param sql: Whether to enable SQL support. Default is False.
+        :type sql: bool
+        :param auto_sql: Whether to automatically update the database after every state change. Default is False.
+        :type auto_sql: bool
+        :param db_conn_str: Database connection string. Default is None.
+        :type db_conn_str: str or None
+        :param db_name: Database name. Default is None.
+        :type db_name: str or None
+        :param db_user: Database username. Default is None.
+        :type db_user: str or None
+        :param db_password: Database password. Default is None.
+        :type db_password: str or None
+        :param db_host: Database host address. Default is None.
+        :type db_host: str or None
+        :param db_port: Database port number. Default is None.
+        :type db_port: str or None
+        """
 
         super().__init__(white_id, black_id, group_id, white_name, black_name)
 
@@ -58,7 +125,31 @@ class Game(BaseGame):
     # if save is a string to a filepath, we save a PNG image of the board to the given location
     #   save implies img=True
     def move(self, move, img=False, save=None):
+        """
+        Executes a chess move if it is the active player's turn.
 
+        This method validates the move, applies it to the board, and changes the turn to the other player.
+        The move is validated and applied to the board, changing the turn. Optionally
+        generates a visual representation of the board or saves it as a PNG file.
+        Automatically updates the SQL database if `auto_sql` is enabled.
+
+        :param move: The move to execute, in standard chess notation (e.g., "e4").
+        :type move: str
+        :param img: If `True`, returns a `PIL.Image` object representing the board. Default is `False`.
+        :type img: bool
+        :param save: File path to save the board image as a PNG. If provided, it implies `img=True`.
+        :type save: str
+        :return: None or a `PIL.Image` object if `img=True` or `save` is specified.
+        :rtype: None or PIL.Image
+        :raises ChessError.MoveIntoCheckError: If the move would put the player in check.
+        :raises ChessError.PromotionError: If an invalid promotion is attempted or a promotion is required.
+        :raises ChessError.InvalidCastleError: If an invalid castling move is attempted.
+        :raises ChessError.PieceNotFoundError: If no eligible piece is found for the move.
+        :raises ChessError.MultiplePiecesFoundError: If more than one matching piece is found.
+        :raises ChessError.NothingToCaptureError: If no opposing piece exists on the target square.
+        :raises ChessError.CaptureOwnPieceError: If a piece of the same color exists on the target square.
+        :raises ChessError.PieceOnSquareError: If an allied or opponent’s piece occupies the target square improperly.
+        """
         img = super().move(move, img, save)
 
         # handle SQL updating
@@ -77,6 +168,17 @@ class Game(BaseGame):
     # offers a draw
     # "player_id" refers to the player offering the draw
     def draw_offer(self, player_id):
+        """
+        Offers a draw in the current game.
+
+        Updates the SQL database with the draw offer if `auto_sql` is enabled.
+        If both players offer a draw, the game instantly ends in a stalemate.
+
+        :param player_id: The ID of the player offering the draw.
+        :type player_id: int
+        :raises ChessError.DrawAlreadyOfferedError: If the same player has already made a draw offer.
+        :raises ChessError.DrawWrongTurnError: If the player offers a draw out of turn.
+        """
 
         super().draw_offer(player_id)
 
@@ -89,7 +191,15 @@ class Game(BaseGame):
     # checks if a draw exists and accepts if offered
     # "player_id" refers to the player offering the draw
     def draw_accept(self, player_id):
+        """
+        Accepts the opponent's existing draw offer.
 
+        Marks the game as a draw. Automatically removes the game from the database if `auto_sql` is enabled.
+
+        :param player_id: The player accepting the draw offer.
+        :type player_id: int
+        :raises GameError.DrawNotOfferedError: If no draw offer exists.
+        """
         super().draw_accept(player_id)
 
         self.end_check()
@@ -97,7 +207,15 @@ class Game(BaseGame):
     # checks if a draw exists and declines if offered
     # "player_id" refers to the player offering the draw
     def draw_decline(self, player_id):
+        """
+        Declines an active draw request.
 
+        Updates the game state, removing the draw request. Automatically updates the database if `auto_sql` is enabled.
+
+        :param player_id: The player who declines the draw.
+        :type player_id: int
+        :raises ChessError.DrawNotOfferedError: If there is no draw request to decline.
+        """
         super().draw_decline(player_id)
 
         if self.auto_sql:
@@ -108,15 +226,29 @@ class Game(BaseGame):
 
     # checks if the game is over and deletes the game from the database accordingly
     def end_check(self):
+        """
+        Checks if the game is over, either by draw or checkmate, and removes it from the SQL database if finished.
+
+        :return: True if the game is over, False otherwise.
+        :rtype: bool
+        """
         if self.board.status != 0:
             if self.auto_sql:
                 self.sql_delete_game(self.gid, self.wid, self.bid)
             return True
         return False
 
-    # This is the function for updating the database, for cases where sql=true but auto_sql=false
-    # returns False and does nothing if sql is not enabled
     def update_db(self):
+        """
+        Updates the database with the current game state.
+
+        This method is designed for cases where `sql=True` but `auto_sql=False`.
+        If SQL support is enabled (`sql` or `auto_sql`), updates the game's record in the database
+        with the current board state, turn, draw status, and player information.
+
+        :return: True if the update was successful, False if SQL support is not enabled.
+        :rtype: bool
+        """
         if self.sql or self.auto_sql:
             execute_sql(f"""
             UPDATE Games SET
@@ -134,12 +266,28 @@ class Game(BaseGame):
             return False
 
     def db_init(self):
+        """
+        Initializes the database for game storage and interaction.
+
+        This method sets up the connection to the database using the credentials provided through
+        environment variables or during the instantiation of the `Game` object.
+        Typically used to ensure the database schema is initialized or available for interaction.
+        """
         sql_db_init(prog_sql_creds=self.sql_creds)
 
     # checks the sql data if a game exists
     #   if it does, get the game data from the database
     #   if it doesn't, create the game and return the new game data
     def sql_game_init(self, white_id, black_id, group_id=0, white_name='', black_name=''):
+        """
+        Initializes or loads a game from the SQL database.
+
+        If the specified players are not already in a game, a new record is created in the database.
+        Otherwise, the existing game state is retrieved.
+
+        :return: Tuple containing game data: board state, turn, draw status, pawn state.
+        :rtype: tuple
+        """
         game = execute_sql(f"""
             DO $$
             BEGIN
@@ -162,10 +310,21 @@ class Game(BaseGame):
         return boardarray, turn, draw, two_move_p
 
 
-    def sql_current_games(self, player_id, gid=0):
+    def sql_current_games(self, player_id: int, gid: int = 0):
+        """
+        Provides the list of current active games for a player from the SQL database.
 
-        games = execute_sql(f"""
-            WITH PlayerResult AS (
+        This method queries the database to find active games associated with the given player ID within the specified group.
+        It determines opponents based on whether the player ID is set as WhiteId or BlackId in the database.
+
+        :param player_id: The ID of the player for whom active games are being retrieved.
+        :type player_id: int
+        :param gid: The group ID for the games. Default is 0.
+        :type gid: int
+        :return: A list of IDs of opponents currently involved in active games with the given player.
+        :rtype: list[int]
+        """
+        games = execute_sql("""WITH PlayerResult AS (
                 SELECT 
                     CASE 
                         WHEN WhiteId = {player_id} THEN BlackId
@@ -185,7 +344,18 @@ class Game(BaseGame):
     # if the game exists, returns the white player's id and black player's id in that order
     # returns False if the game is not found in the database
     def sql_game_exists(self, player1, player2, gid=0):
+        """
+        Checks if a game already exists between two players in the database.
 
+        :param player1: The ID of the first player.
+        :type player1: int
+        :param player2: The ID of the second player.
+        :type player2: int
+        :param gid: The ID of the group in which the players are playing.
+        :type gid: int
+        :return: Tuple of player IDs if the game exists, otherwise False.
+        :rtype: tuple | bool
+        """
         games = execute_sql(f"""
             SELECT WhiteId, BlackId FROM Games 
             WHERE GroupId = {gid} AND (
@@ -201,6 +371,16 @@ class Game(BaseGame):
 
     # removes a game from the database
     def sql_delete_game(self, wid, bid, gid=0):
+        """
+        Deletes the specified game from the database.
+
+        :param white_id: The ID of the white player.
+        :type white_id: int
+        :param black_id: The ID of the black player.
+        :type black_id: int
+        :param gid: The ID of the group.
+        :type gid: int
+        """
         execute_sql(f"DELETE FROM games WHERE GroupId = {gid} and WhiteId = {wid} and BlackId = {bid}",
                     prog_sql_creds=self.sql_creds)
 
@@ -208,8 +388,29 @@ class Game(BaseGame):
 # the SQL wrapper function for the Challenges table
 # This class is not compatible with non-SQL games
 class Challenge:
+    """
+    SQL wrapper for challenges recorded in the `Challenges` SQL table.
+
+    Allows issuing, validating, and deleting game challenges between players.
+
+    Currently only supports databases will envs for database creds.
+    """
     @staticmethod
     def challenge(challenger=0, opponent=1, gid=0):
+        """
+        Issues a chess game challenge between two players.
+
+        Validates that no active game or challenge exists between the players before creating a new challenge.
+        If a reciprocal challenge exists, the challenge is accepted, and it proceeds to game creation.
+
+        :param challenger: ID of the player making the challenge.
+        :type challenger: int
+        :param opponent: ID of the player being challenged.
+        :type opponent: int
+        :param gid: Group ID. Default is 0.
+        :type gid: int
+        :raises GameError.ChallengeError: If the challenge cannot be created.
+        """
 
         if challenger == opponent:
             raise GameError.ChallengeError("You can't challenge yourself, silly")
@@ -234,6 +435,18 @@ class Challenge:
     # otherwise, returns False
     @staticmethod
     def exists(player1, player2, gid=0):
+        """
+        Checks if a challenge exists between two players.
+
+        :param player1: First player ID.
+        :type player1: int
+        :param player2: Second player ID.
+        :type player2: int
+        :param gid: Group ID. Default is 0.
+        :type gid: int
+        :return: Tuple `(challenger, challenged)` if a challenge exists. Otherwise, False.
+        :rtype: tuple | bool
+        """
         games = execute_sql(f"""
             SELECT Challenger, Challenged FROM Challenges 
             WHERE GroupId = {gid} AND (
@@ -250,6 +463,16 @@ class Challenge:
     # if the challenge does not exist in the database, a new one is created
     @staticmethod
     def create_challenge(challenger, challenged, gid=0):
+        """
+        Creates a new challenge between two players in the database.
+
+        :param challenger: The ID of the player issuing the challenge.
+        :type challenger: int
+        :param challenged: The ID of the player being challenged.
+        :type challenged: int
+        :param gid: Group ID. Default is 0.
+        :type gid: int
+        """
         execute_sql(f"""
             DO $$
             BEGIN
@@ -263,4 +486,14 @@ class Challenge:
     # if the challenge exists in the database, it is deleted
     @staticmethod
     def delete_challenge(challenger, challenged, gid=0):
+        """
+        Deletes an existing challenge between two players from the database.
+
+        :param challenger: The ID of the player who issued the challenge.
+        :type challenger: int
+        :param challenged: The ID of the player who was challenged.
+        :type challenged: int
+        :param gid: Group ID. Default is 0.
+        :type gid: int
+        """
         execute_sql(f"DELETE FROM Challenges WHERE GroupId={gid} and Challenger={challenger} and Challenged={challenged}")
